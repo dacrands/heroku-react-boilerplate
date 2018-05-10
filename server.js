bae999f0793d850f6185c778a02a129ca7815dd9
David Crandall <davecrands@gmail.com>
2018-05-10 11:06:31 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const webpack  = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('./webpack.prod.js')
+
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+}));
+
+app.set('port',' process.env.PORT || 8888')
+
+app.listen(app.get('port'), function() {
+    console.log('Listening on port 3000')
+});
